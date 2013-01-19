e9810a232c6a18ec3ed29586191d179ee8d6244b
Jesús David <jedga_95@outlook.com>
2013-01-19 18:27:45 -0500

    Toggles swapping :)

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -140,8 +140,8 @@ public abstract class BaseStatusBar extends SystemUI implements
     protected FrameLayout mStatusBarContainer;
 
     // Pie controls
-    PieControlPanel mPieControlPanel;
-    View mPieControlsTrigger;
+    public PieControlPanel mPieControlPanel;
+    public View mPieControlsTrigger;
 
     // Policy
     public NetworkController mNetworkController;
@@ -639,7 +639,8 @@ public abstract class BaseStatusBar extends SystemUI implements
 
     @Override
     public void animateCollapsePanels(int flags) {
-        if (mPieControlPanel != null && flags == CommandQueue.FLAG_EXCLUDE_NONE) {
+        if (mPieControlPanel != null
+                && flags == CommandQueue.FLAG_EXCLUDE_NONE) {
             mPieControlPanel.animateCollapsePanels();
         }
     }
