45ab0ed04b44947bef94ed33ab0f94f261fc6ed9
htchoi1995 <htchoi1995@gmail.com>
2013-02-05 13:50:11 +0900

    Fix build
    Signed-off-by: htchoi1995 <htchoi1995@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3766,6 +3766,12 @@ public final class Settings {
          */
         @Deprecated
         public static final String ADB_ENABLED = Global.ADB_ENABLED;
+        
+        /**
+         * The TCP/IP port to run ADB on, or -1 for USB
+         * @hide
+         */
+        public static final String ADB_PORT = "adb_port";
 
         /**
          * Whether to display the ADB notification.
