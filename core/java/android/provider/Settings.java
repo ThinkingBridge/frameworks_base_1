d6d80dedf3773839a035f1ccf4197f02a3fa7189
drcmda <drcmda@gmail.com>
2013-02-25 16:50:22 +0000

    PIE: compressed layout, stubs, new defaults
    Change-Id: I6572adef19ee608fd09dc9f56e1b80123ede2b3d

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2882,6 +2882,7 @@ public final class Settings {
         /**
          * Whether to show statusbar signal text
          *
+         * Pie menu, should default to 1 (yes, show)
          * @hide
          */
         public static final String STATUSBAR_SIGNAL_TEXT = "statusbar_signal_text";
@@ -2913,7 +2914,7 @@ public final class Settings {
         public static final String PIE_SEARCH = "pie_search";
 
         /**
-         * Pie will not rotate. Should default to 0 (no, rotate)
+         * Pie will not rotate. Should default to 1 (yes, do not rotate)
          * @hide
          */
         public static final String PIE_STICK = "pie_stick";
@@ -2925,7 +2926,7 @@ public final class Settings {
         public static final String PIE_LAST_APP = "pie_last_app";
 
         /**
-         * Pie gap angle, should default to 1
+         * Pie gap angle, should default to 3
          * @hide
          */
         public static final String PIE_GAP = "pie_gap";
